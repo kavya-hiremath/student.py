958227c38b97b4a9f3f9be3c8c8c54ff8f176f77
Kavya <234810382+kavya-hiremath@users.noreply.github.com>
2025-11-20 14:34:12 +0530

    student.py
    Fix typo in usage message for script execution.

---
+++ student.py
@@ -0,0 +1,14 @@
+import sys
+      # Check if correct number of arguments
+if len(sys.argv) !=3:
+    print ("usage: pthon student.py <name> <rollno>)
+sys.exit(1)
+
+#sys.argv[0] is always the program name
+script_name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
+
+print("Script Name:",script_name)
+print("Student Name:",name)
+print("Roll Number:",rollno)
